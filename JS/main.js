2634dd8b7fc574df3bfbbc6aa1d586146b8259db
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-25 23:48:15 +0300

    hot deals section ==> make all section responsive, show only products with sale

--- JS/main.js
+++ JS/main.js
@@ -25,54 +25,52 @@ async function getProducts() {
   let data = await fetch("../products.json");
   let products = await data.json();
   console.log(products);
-  displayHomeProducts(products);
+  displayHotProducts(products);
 }
 
 getProducts();
 
-function displayHomeProducts(products) {
-  let product = ``;
-  for (let i = 0; i < products.length; i++) {
-    product += `
-          <div class="swiper-slide product-card">
-              <span class="sale">50%</span>
+function displayHotProducts(product) {
+  let item = ``;
+  for (let i = 0; i < product.length; i++) {
+    if (product[i].old_price) {
+      let discount = Math.floor((product[i].old_price - product[i].price) / product[i].old_price * 100)
+      item += `
+      <div class="swiper-slide product-card">
+          <span class="sale">${discount}%</span>
 
-              <div class="product-image">
-                <img src="${products[i].img}" alt="" />
-              </div>
+          <div class="product-image">
+            <img src="${product[i].img}" alt="" />
+          </div>
 
-              <div class="rating">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
+          <div class="rating">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
 
-              <h3 class="title">
-                <a href="#">${products[i].name
-                  .split(" ")
-                  .slice(0, 6)
-                  .join(" ")}</a>
-              </h3>
+          <h3 class="title">
+            <a href="#">${product[i].name.split(" ").slice(0, 6).join(" ")}</a>
+          </h3>
 
-              <div class="price">
-                <p class="new">$${products[i].price}</p>
-                <p class="old">$${products[i].price * 2}</p>
-              </div>
+          <div class="price">
+            <p class="new">$${product[i].price}</p>
+            <p class="old">$${product[i].old_price}</p>
+          </div>
 
-              <div class="btns">
-                <button class="add-to-cart">
-                  <i class="fa-solid fa-cart-shopping"></i>Add To Cart
-                </button>
-                <button class="wish">
-                  <i class="fa-regular fa-heart"></i>
-                </button>
-              </div>
-            </div>
-    
-    `;
+          <div class="btns">
+            <button class="add-to-cart">
+              <i class="fa-solid fa-cart-shopping"></i>Add To Cart
+            </button>
+            <button class="wish">
+              <i class="fa-regular fa-heart"></i>
+            </button>
+          </div>
+      </div>
+      `;
+    }
   }
-
-  hotProductsContainer.innerHTML = product;
+  hotProductsContainer.innerHTML = item;
 }
--- JS/swiper.js
+++ JS/swiper.js
@@ -11,18 +11,35 @@ var swiper = new Swiper(".slide-swp", {
   loop: true,
 });
 
-
 // Initialize Swiper products
 
 var swiper = new Swiper(".product-slide", {
-  slidesPerView: 5,
-  spaceBetween: 20,
+  // slidesPerView: 5,
+  // spaceBetween: 20,
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    prevEl: ".swiper-button-prev",
   },
   autoplay: {
     delay: 2500,
   },
   loop: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    450: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
 });
