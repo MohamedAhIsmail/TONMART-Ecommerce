d945b72e715f0f3d70310de99fcf55df0c1652a9
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-25 03:24:13 +0300

    finished bottom header => categories dropdown &  nav menu & login and sign up buttons & open drawer menu om small screens

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,25 @@
+//handel header functions
+let sideMenu = document.querySelector(".menu");
+let menuIcon = document.querySelector(".menu-icon");
+let closeBtn = document.querySelector(".close");
+let catMenu = document.querySelector(".categories");
+let dropMenu = document.querySelector(".drop-menu");
+
+console.log(catMenu, dropMenu);
+
+menuIcon.addEventListener("click", () => {
+  sideMenu.style.left = "-46px";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.left = "-350px";
+});
+
+catMenu.addEventListener("click", ()=> {
+  dropMenu.classList.toggle("toggled")
+})
+
+// get products
 async function getProducts() {
   let data = await fetch("../products.json");
   let products = await data.json();
