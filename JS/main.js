2a0c8a59825f132e91b9aee2f749c5197d2096c4
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-26 23:57:25 +0300

    enhanced ==> code and functions

--- JS/main.js
+++ JS/main.js
@@ -26,13 +26,24 @@ catMenu.addEventListener("click", () => {
 
 // fetch products and display categories in home
 async function getProducts() {
-  let data = await fetch("../products.json");
-  let products = await data.json();
-  console.log(products);
-  displayHotProducts(products);
-  displayElectronics(products);
-  displayAppliances(products);
-  displayMobiles(products);
+  try {
+    let response = await fetch("../products.json");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    let products = await response.json();
+    console.log(products);
+
+    displayHotProducts(products);
+    displayCategoryProducts(products, electronicsContainer, "electronics");
+    displayCategoryProducts(products, appliancesContainer, "appliances");
+    displayCategoryProducts(products, mobilesContainer, "mobiles");
+    
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+  }
 }
 
 getProducts();
@@ -84,10 +95,10 @@ function displayHotProducts(product) {
   hotProductsContainer.innerHTML = item;
 }
 
-function displayElectronics(product) {
+function displayCategoryProducts(product, container, category) {
   let item = ``;
   for (let i = 0; i < product.length; i++) {
-    if (product[i].catetory === "electronics") {
+    if (product[i].category === category) {
       item += `
       <div class="swiper-slide product-card">
 
@@ -123,95 +134,10 @@ function displayElectronics(product) {
       `;
     }
   }
-  electronicsContainer.innerHTML = item;
-}
-
-function displayAppliances(product) {
-  let item = ``;
-  for (let i = 0; i < product.length; i++) {
-    if (product[i].catetory === "appliances") {
-      item += `
-      <div class="swiper-slide product-card">
-
-          <div class="product-image">
-            <img src="${product[i].img}" alt="" />
-          </div>
-
-          <div class="rating">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-          </div>
-
-          <h3 class="title">
-            <a href="#">${product[i].name}</a>
-          </h3>
-
-          <div class="price">
-            <p class="new">$${product[i].price}</p>
-          </div>
-
-          <div class="btns">
-            <button class="add-to-cart">
-              <i class="fa-solid fa-cart-shopping"></i>Add To Cart
-            </button>
-            <button class="wish">
-              <i class="fa-regular fa-heart"></i>
-            </button>
-          </div>
-      </div>
-      `;
-    }
-  }
-  appliancesContainer.innerHTML = item;
-}
-
-function displayMobiles(product) {
-  let item = ``;
-  for (let i = 0; i < product.length; i++) {
-    if (product[i].catetory === "mobiles") {
-      item += `
-      <div class="swiper-slide product-card">
-
-          <div class="product-image">
-            <img src="${product[i].img}" alt="" />
-          </div>
-
-          <div class="rating">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-          </div>
-
-          <h3 class="title">
-            <a href="#">${product[i].name}</a>
-          </h3>
-
-          <div class="price">
-            <p class="new">$${product[i].price}</p>
-          </div>
-
-          <div class="btns">
-            <button class="add-to-cart">
-              <i class="fa-solid fa-cart-shopping"></i>Add To Cart
-            </button>
-            <button class="wish">
-              <i class="fa-regular fa-heart"></i>
-            </button>
-          </div>
-      </div>
-      `;
-    }
-  }
-  mobilesContainer.innerHTML = item;
+  container.innerHTML = item;
 }
 
 // To Top button feature
-
 let toTopBtn = document.querySelector(".to-top");
 console.log(toTopBtn);
 
