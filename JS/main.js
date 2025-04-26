9d3ed4759ee4385276bcd40ae9caa09d428018df
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-26 20:01:07 +0300

    Appliances Section ==> show only products with category Appliances, and ads banners section

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,7 @@ let catMenu = document.querySelector(".categories");
 let dropMenu = document.querySelector(".drop-menu");
 let hotProductsContainer = document.querySelector(".products");
 let electronicsContainer = document.getElementById("swiper-electronics");
+let appliancesContainer = document.getElementById("swiper-appliances");
 
 console.log(electronicsContainer);
 
@@ -28,6 +29,7 @@ async function getProducts() {
   console.log(products);
   displayHotProducts(products);
   displayElectronics(products);
+  displayAppliances(products);
 }
 
 getProducts();
@@ -120,4 +122,45 @@ function displayElectronics(product) {
   electronicsContainer.innerHTML = item;
 }
 
+function displayAppliances(product) {
+  let item = ``;
+  for (let i = 0; i < product.length; i++) {
+    if (product[i].catetory === "appliances") {
+      
+      item += `
+      <div class="swiper-slide product-card">
 
+          <div class="product-image">
+            <img src="${product[i].img}" alt="" />
+          </div>
+
+          <div class="rating">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+
+          <h3 class="title">
+            <a href="#">${product[i].name.split(" ").slice(0, 6).join(" ")}</a>
+          </h3>
+
+          <div class="price">
+            <p class="new">$${product[i].price}</p>
+          </div>
+
+          <div class="btns">
+            <button class="add-to-cart">
+              <i class="fa-solid fa-cart-shopping"></i>Add To Cart
+            </button>
+            <button class="wish">
+              <i class="fa-regular fa-heart"></i>
+            </button>
+          </div>
+      </div>
+      `;
+    }
+  }
+  appliancesContainer.innerHTML = item;
+}
