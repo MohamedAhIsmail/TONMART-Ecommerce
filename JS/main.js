52e9715dae83208b955c25d189789fd78f271067
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-27 04:15:30 +0300

    wish list ==> finsished show wish list items, wished items count, remove item from wish list, save and updated in local storage

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,7 @@ let hotProductsContainer = document.querySelector(".products");
 let electronicsContainer = document.getElementById("swiper-electronics");
 let appliancesContainer = document.getElementById("swiper-appliances");
 let mobilesContainer = document.getElementById("swiper-mobiles");
+let wishCounter = document.querySelector(".wish-count");
 
 let products = [];
 
@@ -16,6 +17,8 @@ let wishList = [];
 if (window.localStorage.getItem("wishList")) {
   wishList = JSON.parse(window.localStorage.getItem("wishList"));
 }
+
+wishCounter.innerHTML = wishList.length;
 
 // console.log(electronicsContainer);
 
@@ -184,6 +187,9 @@ function addToWish(id) {
     displayCategoryProducts(products, electronicsContainer, "electronics");
     displayCategoryProducts(products, appliancesContainer, "appliances");
     displayCategoryProducts(products, mobilesContainer, "mobiles");
+
+    showWishListItems();
+    updateWishCounter();
   }
 }
 
@@ -191,13 +197,13 @@ function saveWishListData() {
   window.localStorage.setItem("wishList", JSON.stringify(wishList));
 }
 
-console.log("wishlist", wishList);
-
 // wish list side menu
 let closeWishBtn = document.getElementById("close-wish");
 let wishListSide = document.querySelector(".wish-list");
 let shopMore = document.querySelector(".shop-more");
 let wishHeart = document.getElementById("wish");
+let wishListContainer = document.querySelector(".list");
+let wishedItemsCounter = document.querySelector(".wish-list .top h3");
 
 function closeWishList() {
   wishListSide.style.setProperty("transform", "translateX(350px)");
@@ -210,3 +216,40 @@ function openWishList() {
 wishHeart.addEventListener("click", openWishList);
 closeWishBtn.addEventListener("click", closeWishList);
 shopMore.addEventListener("click", closeWishList);
+
+function showWishListItems() {
+  let wishItem = ``;
+
+  for (let i = 0; i < wishList.length; i++) {
+    wishItem += `
+        <div class="item">
+          <img src="${wishList[i].img}" alt="${wishList[i].name}" />
+          <div>
+            <h4 class="title">
+            ${wishList[i].name}
+            </h4>
+            <div class="price">$${wishList[i].price}</div>
+          </div>
+          <button class="delete" onclick="removeFromWish(${wishList[i].id})"><i class="fa-solid fa-trash-can"></i></button>
+        </div>
+    
+    `;
+  }
+
+  wishListContainer.innerHTML = wishItem;
+  wishedItemsCounter.innerHTML = `Your Wish List Items: ${wishList.length}`;
+}
+
+showWishListItems();
+
+function removeFromWish(id) {
+  wishList = wishList.filter((product) => product.id != id);
+  showWishListItems();
+  updateWishCounter();
+  saveWishListData();
+}
+
+function updateWishCounter() {
+  wishCounter.innerHTML = wishList.length;
+  wishedItemsCounter.innerHTML = `Your Wish List Items: ${wishList.length}`;
+}
